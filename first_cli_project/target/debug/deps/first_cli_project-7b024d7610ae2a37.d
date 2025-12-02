56b9240989fc2924618a4420f0adda42e2966ee3
ritik chahar <ritikchahar56@gmail.com>
2025-12-02 13:47:37 +0530

    new cli project

---
+++ first_cli_project/target/debug/deps/first_cli_project-7b024d7610ae2a37.d
@@ -0,0 +1,5 @@
+/Users/mr.chahar/Rustacean/mini_projects/first_cli_project/target/debug/deps/first_cli_project-7b024d7610ae2a37.d: src/main.rs
+
+/Users/mr.chahar/Rustacean/mini_projects/first_cli_project/target/debug/deps/libfirst_cli_project-7b024d7610ae2a37.rmeta: src/main.rs
+
+src/main.rs:
---
+++ first_cli_project/target/debug/deps/serde-6edcdce38c82962f.d
@@ -0,0 +1,12 @@
+/Users/mr.chahar/Rustacean/mini_projects/first_cli_project/target/debug/deps/serde-6edcdce38c82962f.d: /Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/lib.rs /Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/integer128.rs /Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/private/mod.rs /Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/private/de.rs /Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/private/ser.rs /Users/mr.chahar/Rustacean/mini_projects/first_cli_project/target/debug/build/serde-8c3a8118be904d2c/out/private.rs
+
+/Users/mr.chahar/Rustacean/mini_projects/first_cli_project/target/debug/deps/libserde-6edcdce38c82962f.rmeta: /Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/lib.rs /Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/integer128.rs /Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/private/mod.rs /Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/private/de.rs /Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/private/ser.rs /Users/mr.chahar/Rustacean/mini_projects/first_cli_project/target/debug/build/serde-8c3a8118be904d2c/out/private.rs
+
+/Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/lib.rs:
+/Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/integer128.rs:
+/Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/private/mod.rs:
+/Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/private/de.rs:
+/Users/mr.chahar/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/src/private/ser.rs:
+/Users/mr.chahar/Rustacean/mini_projects/first_cli_project/target/debug/build/serde-8c3a8118be904d2c/out/private.rs:
+
+# env-dep:OUT_DIR=/Users/mr.chahar/Rustacean/mini_projects/first_cli_project/target/debug/build/serde-8c3a8118be904d2c/out

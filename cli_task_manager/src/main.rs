f8f03b00f8827c546f15357c9b9f2d742b8fc024
ritik chahar <ritikchahar56@gmail.com>
2025-12-03 15:34:45 +0530

    Updated code

--- cli_task_manager/src/main.rs
+++ cli_task_manager/src/main.rs
@@ -1,15 +1,23 @@
 mod cli;
+mod storage;
 
-
+use storage::Task;
 use clap::Parser;
 use cli::Cli;
 fn main() {
   let cli = Cli::parse();
+  let mut tasks = storage::load_task();
       match cli.command{
         cli::Commands::Add { task, is_done } => {
          let status =  if is_done {"Done"} else {"Undone"};
-          println!("Task : {} , Status : {}" ,task,status);
-         },
+          tasks.push(Task{
+            task : task,
+            is_done : status.to_string()
+          });
+          storage::save_task(&tasks);
+          for c in &tasks{
+          println!("Task : {} , Status : {}" ,c.task,c.is_done);
+         } },
           }
 
 }
---
+++ cli_task_manager/target/debug/deps/cli_task_manager-e586ecbeb2579dae.d
@@ -0,0 +1,7 @@
+/Users/mr.chahar/Rustacean/mini_projects/cli_task_manager/target/debug/deps/cli_task_manager-e586ecbeb2579dae.d: src/main.rs src/cli.rs src/storage.rs
+
+/Users/mr.chahar/Rustacean/mini_projects/cli_task_manager/target/debug/deps/cli_task_manager-e586ecbeb2579dae: src/main.rs src/cli.rs src/storage.rs
+
+src/main.rs:
+src/cli.rs:
+src/storage.rs:

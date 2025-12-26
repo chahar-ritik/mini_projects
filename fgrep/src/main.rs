0b8c39013b59170f3bf4f71e09c156447774092c
ritik chahar <ritikchahar156@gmail.com>
2025-12-26 20:58:53 +0530

    fix:  redirect debug output to stderr

--- fgrep/src/main.rs
+++ fgrep/src/main.rs
@@ -6,9 +6,9 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        // eprintln! this is a standard error that not like normal print as it standard output
-        // if we run command cargo run > output.txt that redirect output to this file instead of screen
-        // and this command has error as no argument supplied but standart output will not show it on screen
+        /* eprintln! this is a standard error that not like normal print as it standard output
+         if we run command cargo run > output.txt that redirect output to this file instead of screen
+         and this command has error as no argument supplied but standart output will not show it on screen */
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });

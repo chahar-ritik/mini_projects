52ef7bed14652afe18dc3399572f4e8d3897ef9e
ritik chahar <ritikchahar156@gmail.com>
2025-12-26 15:27:21 +0530

    feat: add test module

--- fgrep/src/lib.rs
+++ fgrep/src/lib.rs
@@ -26,8 +26,39 @@ pub fn run(config:Config) -> Result<(),Box<dyn Error>>{
    
       let content = fs::read_to_string(config.file_name)?;
       
-      println!("With text:\n{content}");
+    for line in search(&config.search_string, &content) {
+        println!("{line}");
+    }
 
         Ok(())
 
+}
+
+fn search<'a>(search_string : &str , file_name :  & 'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+    
+    for line in file_name.lines(){
+      if line.contains(search_string){
+         results.push(line);
+      }
+    }
+    results
+}
+
+
+#[cfg(test)]
+mod tests{
+  use super::*;
+
+
+  #[test]
+  fn res(){
+    let search_string = "duct";
+    let file_name  = "\
+    Rust:
+Safe,fast, productive";
+
+    assert_eq!(vec!["Safe,fast, productive"],search(search_string , file_name))
+
+  }
 }

3a83f0373001088ef90c7cbe568cde632506099a
ritik chahar <ritikchahar156@gmail.com>
2025-12-26 20:56:35 +0530

    fix: standart error for errors

--- fgrep/src/lib.rs
+++ fgrep/src/lib.rs
@@ -1,102 +1,97 @@
-use std::fs;
-use std::error::Error;
 use std::env;
+use std::error::Error;
+use std::fs;
 pub struct Config {
-
-   pub search_string : String,
-   pub file_name : String,
-   pub ignore_case : bool
-
+    pub search_string: String,
+    pub file_name: String,
+    pub ignore_case: bool,
 }
 
- 
-impl Config{
+impl Config {
     // in place of new we make build method because new are not expected to fail
-   pub fn build(args : &[String]) -> Result<Config,&'static str>{
-      if args.len() < 3{
-          return Err("not enough arguments")
-      } 
-      let search = args[1].clone();
-       let file  = args[2].clone();
-       //IGNORE_CASE=1 cargo run -- to myfile.txt
-       //Here we are using IGNOR_CASE to set variable value true by 1 or empty
-       let ignore_case = env::var("IGNORE_CASE").is_ok();
-      Ok(Config{search_string: search , file_name: file , ignore_case})
-
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let search = args[1].clone();
+        let file = args[2].clone();
+        //IGNORE_CASE=1 cargo run -- to myfile.txt
+        //Here we are using IGNOR_CASE to set variable value true by 1 or empty
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config {
+            search_string: search,
+            file_name: file,
+            ignore_case,
+        })
     }
 }
 
-pub fn run(config:Config) -> Result<(),Box<dyn Error>>{
-     
-      let content = fs::read_to_string(&config.file_name)?;
-  
- 
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(&config.file_name)?;
 
-      let result = if config.ignore_case {   
-      search_case_insensitive(&config.search_string, &content)
-     
-      }
-   else { 
+    let result = if config.ignore_case {
+        search_case_insensitive(&config.search_string, &content)
+    } else {
         search(&config.search_string, &content)
-       
     };
-   
-      for i in result{
-        println!("{i}");
-      }
-        Ok(())
 
+    for i in result {
+        println!("{i}");
+    }
+    Ok(())
 }
 
-fn search<'a>(search_string : &str , content :  & 'a str) -> Vec<&'a str>{
+fn search<'a>(search_string: &str, content: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
-    
-    for line in content.lines(){
-      if line.contains(search_string){
-         results.push(line);
-      }
+
+    for line in content.lines() {
+        if line.contains(search_string) {
+            results.push(line);
+        }
     }
     results
 }
 
-
-fn search_case_insensitive<'a>(search_string : &str , content :  & 'a str) -> Vec<&'a str>{
+fn search_case_insensitive<'a>(search_string: &str, content: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
-   let search_string = search_string.to_lowercase(); 
-    
-    for line in content.lines(){
-      if line.to_lowercase().contains(&search_string){
-         results.push(line);
-      }
+    let search_string = search_string.to_lowercase();
+
+    for line in content.lines() {
+        if line.to_lowercase().contains(&search_string) {
+            results.push(line);
+        }
     }
     results
 }
 
 #[cfg(test)]
-mod tests{
-  use super::*;
+mod tests {
+    use super::*;
 
-
-  #[test]
-  fn case_sensitive(){
-    let search_string = "duct";
-    let content  = "\
+    #[test]
+    fn case_sensitive() {
+        let search_string = "duct";
+        let content = "\
     Rust:
 Safe,fast, productive ,Duct";
 
-    assert_eq!(vec!["Safe,fast, productive"],search(search_string , content))
+        assert_eq!(
+            vec!["Safe,fast, productive"],
+            search(search_string, content)
+        )
+    }
 
-  }
-
-  #[test]
-  fn case_insensitive(){
- let search_string = "duct";
-    let content  = "\
+    #[test]
+    fn case_insensitive() {
+        let search_string = "duct";
+        let content = "\
     Rust:
 Safe,fast, productive
 Trust";
 
-    assert_eq!(vec!["Rust:","Trust"],search_case_insensitive(search_string , content))
-
-  }
+        assert_eq!(
+            vec!["Rust:", "Trust"],
+            search_case_insensitive(search_string, content)
+        )
+    }
 }
--- fgrep/src/main.rs
+++ fgrep/src/main.rs
@@ -1,23 +1,23 @@
+use fgrep::Config;
 use std::env;
 use std::process;
-use fgrep::Config;
 
-fn main(){
+fn main() {
     let args: Vec<String> = env::args().collect();
-     
-       let config = Config::build(&args).unwrap_or_else(|err|{
-         println!("Problem parsing arguments: {err}");
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        // eprintln! this is a standard error that not like normal print as it standard output
+        // if we run command cargo run > output.txt that redirect output to this file instead of screen
+        // and this command has error as no argument supplied but standart output will not show it on screen
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
-       });
+    });
 
-      println!("Searching for {}",config.search_string);
-    println!("In file {}",config.file_name);
-   
+    println!("Searching for {}", config.search_string);
+    println!("In file {}", config.file_name);
 
-
- if let Err(e) =  fgrep::run(config){
-    println!("Application error: {e}");
-     process::exit(1);// if code O success , 1 failure
- };
-
+    if let Err(e) = fgrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    };
 }
